fec79b6641396217c247200ce8c7c85deaf8926c
Geneviève Bastien <gbastien@versatic.net>
2021-04-26 16:23:21 -0400

    cache_fetcher: scenarios: add checks and return int
    And add unit tests for various cases of valid, invalid and unexinsting
    files when getting scenarios.

---
+++ tests/cache_fetch/scenarios_cache_fetcher_test.cpp
@@ -0,0 +1,61 @@
+#include <errno.h>
+#include <experimental/filesystem>
+
+#include "gtest/gtest.h" // we will add the path to C preprocessor later
+#include "parameters.hpp"
+#include "cache_fetcher.hpp"
+#include "cache_fetcher_test.hpp"
+#include "scenario.hpp"
+#include "capnp/scenarioCollection.capnp.h"
+
+namespace fs = std::experimental::filesystem;
+
+class ScenarioCacheFetcherFixtureTests : public BaseCacheFetcherFixtureTests
+{
+protected:
+    std::vector<std::unique_ptr<TrRouting::Scenario>> objects;
+    std::map<boost::uuids::uuid, int> objectIndexesByUuid;
+    std::map<boost::uuids::uuid, int> serviceIndexesByUuid;
+    std::map<boost::uuids::uuid, int> lineIndexesByUuid;
+    std::map<boost::uuids::uuid, int> agencyIndexesByUuid;
+    std::map<boost::uuids::uuid, int> nodeIndexesByUuid;
+    std::map<std::string, int> modeIndexesByShortname;
+
+public:
+    void SetUp( ) override
+    {
+        BaseCacheFetcherFixtureTests::SetUp();
+        // Copy the invalid file
+        fs::copy_file(PROJECT_NAME + "/" + INVALID_CUSTOM_PATH + "/genericInvalid.capnpbin", PROJECT_NAME + "/" + INVALID_CUSTOM_PATH + "/scenarios.capnpbin");
+    }
+
+    void TearDown( ) override
+    {
+        BaseCacheFetcherFixtureTests::TearDown();
+        // Remove the invalid file
+        fs::remove(PROJECT_NAME + "/" + INVALID_CUSTOM_PATH + "/scenarios.capnpbin");
+    }
+};
+
+TEST_F(ScenarioCacheFetcherFixtureTests, TestGetScenariosInvalid)
+{
+    int retVal = cacheFetcher.getScenarios(objects, objectIndexesByUuid, serviceIndexesByUuid, lineIndexesByUuid, agencyIndexesByUuid, nodeIndexesByUuid, modeIndexesByShortname, params, INVALID_CUSTOM_PATH);
+    ASSERT_EQ(-EBADMSG, retVal);
+    ASSERT_EQ(0, objects.size());
+}
+
+// TODO Add tests for various services, lines, agencies that don't exist. But first, we should be able to create cache files with mock test data
+TEST_F(ScenarioCacheFetcherFixtureTests, TestGetScenariosValid)
+{
+    int retVal = cacheFetcher.getScenarios(objects, objectIndexesByUuid, serviceIndexesByUuid, lineIndexesByUuid, agencyIndexesByUuid, nodeIndexesByUuid, modeIndexesByShortname, params, VALID_CUSTOM_PATH);
+    ASSERT_EQ(0, retVal);
+    ASSERT_EQ(2, objects.size());
+}
+
+TEST_F(ScenarioCacheFetcherFixtureTests, TestGetScenariosFileNotExists)
+{
+    int retVal = cacheFetcher.getScenarios(objects, objectIndexesByUuid, serviceIndexesByUuid, lineIndexesByUuid, agencyIndexesByUuid, nodeIndexesByUuid, modeIndexesByShortname, params, BASE_CUSTOM_PATH);
+    ASSERT_EQ(-ENOENT, retVal);
+    ASSERT_EQ(0, objects.size());
+}
+
