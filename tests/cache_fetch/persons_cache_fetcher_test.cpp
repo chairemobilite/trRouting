ca3495ce4ca7e844e40ab44aab6c7f8daf5aa385
Geneviève Bastien <gbastien@versatic.net>
2021-04-28 15:05:09 -0400

    cache_fetcher: persons: add checks and return int
    Add unit tests for default case with persons.
    TODO: Test with data

---
+++ tests/cache_fetch/persons_cache_fetcher_test.cpp
@@ -0,0 +1,30 @@
+#include <errno.h>
+#include <experimental/filesystem>
+
+#include "gtest/gtest.h" // we will add the path to C preprocessor later
+#include "parameters.hpp"
+#include "cache_fetcher.hpp"
+#include "cache_fetcher_test.hpp"
+#include "person.hpp"
+#include "capnp/person.capnp.h"
+
+namespace fs = std::experimental::filesystem;
+
+class PersonCacheFetcherFixtureTests : public BaseCacheFetcherFixtureTests
+{
+protected:
+    std::vector<std::unique_ptr<TrRouting::Person>> objects;
+    std::map<boost::uuids::uuid, int> objectIndexesByUuid;
+    std::map<boost::uuids::uuid, int> dataSourceIndexesByUuid;
+    std::map<boost::uuids::uuid, int> householdIndexesByUuid;
+    std::map<boost::uuids::uuid, int> nodeIndexesByUuid;
+};
+
+TEST_F(PersonCacheFetcherFixtureTests, TestGetPersonsValid)
+{
+    int retVal = cacheFetcher.getPersons(objects, objectIndexesByUuid, dataSourceIndexesByUuid, householdIndexesByUuid, nodeIndexesByUuid, params, VALID_CUSTOM_PATH);
+    ASSERT_EQ(0, retVal);
+    ASSERT_EQ(0, objects.size());
+}
+
+// TODO Test with actual data (and invalid ones)
