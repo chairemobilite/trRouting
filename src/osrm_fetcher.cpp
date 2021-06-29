ce053a04a24bb25d02f20ed6cf0c85b32d26f262
Yasmine Moumou <moumouyasmine@gmail.com>
2021-06-29 19:14:36 -0400

    Add bird distance accessibilitity implementation

--- src/osrm_fetcher.cpp
+++ src/osrm_fetcher.cpp
@@ -27,64 +27,77 @@ namespace TrRouting
     //std::string queryString = "GET /table/v1/" + mode + "/" + std::to_string(point.longitude) +  "," + std::to_string(point.latitude);
     std::string queryString = "/table/v1/" + mode + "/" + std::to_string(point.longitude) +  "," + std::to_string(point.latitude);
 
-    
+    int i {0}; 
 
-    int i {0};
     for (auto & node : nodes)
     {
       //std::cerr << node.point.latitude << std::endl;
       distanceXMeters       = (node->point.get()->longitude - point.longitude) * lengthOfOneDegreeOfLongitude;
       distanceYMeters       = (node->point.get()->latitude  - point.latitude)  * lengthOfOneDegreeOflatitude ;
       distanceMetersSquared = distanceXMeters * distanceXMeters + distanceYMeters * distanceYMeters;
+
       if (distanceMetersSquared <= maxDistanceMetersSquared)
       {
-        birdDistanceAccessibleNodeIndexes.push_back(i);
-        queryString += ";" + std::to_string(node->point.get()->longitude) +  "," + std::to_string(node->point.get()->latitude);
+        if (true) //if (params.birdDistanceAccessibilityEnabled) 
+        {
+          int distanceMeters = sqrt(distanceMetersSquared);
+          int travelTimeSeconds = distanceMeters / params.walkingSpeedMetersPerSecond;
+          accessibleNodesFootpaths.push_back(std::make_tuple(i, travelTimeSeconds, distanceMeters));
+        
+        } else 
+        {
+          birdDistanceAccessibleNodeIndexes.push_back(i);
+          queryString += ";" + std::to_string(node->point.get()->longitude) +  "," + std::to_string(node->point.get()->latitude);
+        }
       }
       i++;
     }
-    
-    queryString += "?annotations=duration,distance";
 
-    if (reversed) 
+    if (false) //if (!params.birdDistanceAccessibilityEnabled) 
     {
-      queryString += "&destinations=0";
-    }
-    else
-    {
-      queryString += "&sources=0";
-    }
-    
-    using HttpClient = SimpleWeb::Client<SimpleWeb::HTTP>;
-    HttpClient client(params.osrmWalkingHost + ":" + params.osrmWalkingPort);
-    auto s = client.request("GET", queryString);
 
-    std::stringstream responseJsonSs;
-    responseJsonSs << s->content.rdbuf();
-    nlohmann::json responseJson = nlohmann::json::parse(responseJsonSs.str());
-    
-    if (responseJson["durations"] != nullptr && responseJson["distances"] != nullptr && responseJson["durations"][0] != nullptr && responseJson["distances"][0] != nullptr)
-    {
-      int numberOfDurations = responseJson["durations"][0].size();
-      //std::cout << "numberOfDurations: " << responseJson["durations"][0].dump(2) << std::endl;
-      int numberOfDistances = responseJson["distances"][0].size();
-      //std::cout << "numberOfDistances: " << responseJson["distances"][0].dump(2) << std::endl;
-      int j = 0;
-      if (numberOfDurations > 0 && numberOfDistances > 0)
+      queryString += "?annotations=duration,distance";
+
+      if (reversed) 
       {
-        int travelTimeSeconds;
-        int distanceMeters;
-        for (int i = 1; i < numberOfDurations; i++) // ignore first (duration with itself)
+        queryString += "&destinations=0";
+      }
+      else
+      {
+        queryString += "&sources=0";
+      }
+      
+      using HttpClient = SimpleWeb::Client<SimpleWeb::HTTP>;
+      HttpClient client(params.osrmWalkingHost + ":" + params.osrmWalkingPort);
+      auto s = client.request("GET", queryString);
+
+      std::stringstream responseJsonSs;
+      responseJsonSs << s->content.rdbuf();
+      nlohmann::json responseJson = nlohmann::json::parse(responseJsonSs.str());
+      
+      if (responseJson["durations"] != nullptr && responseJson["distances"] != nullptr && responseJson["durations"][0] != nullptr && responseJson["distances"][0] != nullptr)
+      {
+        int numberOfDurations = responseJson["durations"][0].size();
+        //std::cout << "numberOfDurations: " << responseJson["durations"][0].dump(2) << std::endl;
+        int numberOfDistances = responseJson["distances"][0].size();
+        //std::cout << "numberOfDistances: " << responseJson["distances"][0].dump(2) << std::endl;
+        int j = 0;
+        if (numberOfDurations > 0 && numberOfDistances > 0)
         {
-          travelTimeSeconds = (int)ceil((float)responseJson["durations"][0][i]);
-          if (travelTimeSeconds <= params.maxAccessWalkingTravelTimeSeconds)
+          int travelTimeSeconds;
+          int distanceMeters;
+          for (int i = 1; i < numberOfDurations; i++) // ignore first (duration with itself)
           {
-            distanceMeters = (int)ceil((float)responseJson["distances"][0][i]);
-            accessibleNodesFootpaths.push_back(std::make_tuple(birdDistanceAccessibleNodeIndexes[i-1], travelTimeSeconds, distanceMeters));
+            travelTimeSeconds = (int)ceil((float)responseJson["durations"][0][i]);
+            if (travelTimeSeconds <= params.maxAccessWalkingTravelTimeSeconds)
+            {
+              distanceMeters = (int)ceil((float)responseJson["distances"][0][i]);
+              accessibleNodesFootpaths.push_back(std::make_tuple(birdDistanceAccessibleNodeIndexes[i-1], travelTimeSeconds, distanceMeters));
+            }
           }
         }
       }
-    }
+    } 
 
     if (params.debugDisplay)
         std::cout << "fetched osrm footpaths (" << accessibleNodesFootpaths.size() << " foopaths found)" << std::endl;
