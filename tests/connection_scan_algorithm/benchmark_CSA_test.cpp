0adf44bd38f966d6be6238db020fbc54f7474e3f
Yasmine Moumou <moumouyasmine@gmail.com>
2021-06-30 13:26:26 -0400

    Add benchmarks for the CSA algorithm
    Fixes #53
    
    This adds a test application to run benchmarks for the CSA algorithm. To
    run it, simply run `make check` and it will build this application and
    run the benchmarks.
    
    The benchmarks require cache data to be available in the
    `tests/connection_scan_algorithm/cache/demo_transition` directory. The
    cache data to match the benchmark can be found here:
    https://nuage.facil.services/s/ntXfzgfBEFDS7M2 Simply unzip in the
    `tests/connection_scan_algorithm/cache` directory. It corresponds to the
    STM's fall 2018 18S_S service.
    
    For now, only one query is benchmarked and hard-coded in the file. The
    results are outputted in the benchmarkResults.txt file.
    
    TODO:
    
    * Move this benchmark application outside the `make check` target to
    instead have a `make bench` target.
    * Parameterize cache queries to benchmark multiple queries for the given
    cache. Could be a .csv file that comes with a cache archive.
    * Document how to do benchmarks.

---
+++ tests/connection_scan_algorithm/benchmark_CSA_test.cpp
@@ -0,0 +1,237 @@
+#include "gtest/gtest.h" // we will add the path to C preprocessor later
+#include "parameters.hpp"
+
+#include "server_http.hpp"
+#include "client_http.hpp"
+
+//Added for the json-example
+#define BOOST_SPIRIT_THREADSAFE
+
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <curses.h>
+#include <bits/stdc++.h>
+#include <chrono>
+#include <iostream>
+#include <fstream> 
+
+#include "toolbox.hpp"
+#include "gtfs_fetcher.hpp"
+#include "csv_fetcher.hpp"
+#include "cache_fetcher.hpp"
+#include "calculation_time.hpp"
+#include "parameters.hpp"
+#include "scenario.hpp"
+#include "calculator.hpp"
+#include "program_options.hpp"
+#include "benchmark_CSA_test.hpp"
+
+using namespace TrRouting;
+
+class BenchmarkCSATests : public ConstantBenchmarkCSATests
+{
+
+protected:
+  int numberOfRequests = 0;
+
+  Parameters setupAlgorithmParams()
+  {
+    Parameters algorithmParams;
+
+    algorithmParams.projectShortname = "demo_transition";
+    algorithmParams.cacheDirectoryPath = "cache";
+    algorithmParams.dataFetcherShortname = "cache";
+    algorithmParams.osrmWalkingPort = "5000";
+    algorithmParams.osrmWalkingHost = "localhost"; //"http://localhost";
+    algorithmParams.osrmCyclingPort = "8000";
+    algorithmParams.osrmCyclingHost = "localhost";
+    algorithmParams.osrmDrivingPort = "7000";
+    algorithmParams.osrmDrivingHost = "localhost";
+    algorithmParams.serverDebugDisplay = false;
+
+    GtfsFetcher gtfsFetcher = GtfsFetcher();
+    algorithmParams.gtfsFetcher = &gtfsFetcher;
+    CsvFetcher csvFetcher = CsvFetcher();
+    algorithmParams.csvFetcher = &csvFetcher;
+    CacheFetcher cacheFetcher = TrRouting::CacheFetcher();
+    algorithmParams.cacheFetcher = &cacheFetcher;
+
+    return algorithmParams;
+  }
+
+  bool updateCalculatorFromCache(Calculator *calculator)
+  {
+    std::cout << "Preparing calculator..." << std::endl;
+    int dataStatus = calculator->prepare();
+
+    if (dataStatus < 0)
+    {
+      std::cout << "Something is wrong with the calculator. Data status: " << dataStatus << std::endl;
+      return false;
+    }
+    if (assertCacheOk(calculator) != 0)
+    {
+      std::cout << "Invalid cache" << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  int assertCacheOk(Calculator *calculator)
+  {
+    if (calculator->countAgencies() == 0)
+    {
+      std::cout << "no agencies";
+      return -1;
+    }
+    else if (calculator->countServices() == 0)
+    {
+      std::cout << "no services";
+      return -1;
+    }
+    else if (calculator->countNodes() == 0)
+    {
+      std::cout << "no nodes";
+      return -1;
+    }
+    else if (calculator->countLines() == 0)
+    {
+      std::cout << "no lines";
+      return -1;
+    }
+    else if (calculator->countPaths() == 0)
+    {
+      std::cout << "no paths";
+      return -1;
+    }
+    else if (calculator->countScenarios() == 0)
+    {
+      std::cout << "no scenarios";
+      return -1;
+    }
+    else if (calculator->countConnections() == 0 || calculator->countTrips() == 0)
+    {
+      std::cout << "no schedules";
+      return -1;
+    }
+    return 0;
+  }
+
+  /**
+   * The benchmarks require cache data to be available in the
+   * tests/connection_scan_algorithm/cache/demo_transition directory. The
+   * cache data to match the benchmark can be found here:
+   * https://nuage.facil.services/s/ntXfzgfBEFDS7M2 Simply unzip in the
+   * tests/connection_scan_algorithm/cache directory. It corresponds to the
+   * STM's fall 2018 18S_S service.
+   * */
+  std::vector<std::string> createCalculationQuery()
+  {
+    std::vector<std::string> parametersWithValues;
+    std::pair<std::string, std::string> queryFields[] = {
+        std::make_pair("destination", "45.55239801892435,-73.57786713522127"),
+        std::make_pair("alternatives", "1"),
+        std::make_pair("scenario_uuid", "ed42d920-0349-4f64-8590-4698056c2734"),
+        std::make_pair("origin", "45.542273017129446,-73.62259417256861"),
+        std::make_pair("max_transfer_travel_time_seconds", "600"),
+        std::make_pair("max_egress_travel_time_seconds", "900"),
+        std::make_pair("max_access_travel_time_seconds", "900"),
+        std::make_pair("departure_time_seconds", "28800"),
+        std::make_pair("min_waiting_time_seconds", "180")};
+
+    for (auto &field : queryFields)
+    {
+      parametersWithValues.push_back(field.first + "=" + field.second);
+    }
+    return parametersWithValues;
+  }
+
+  bool updateCalculatorParams(Calculator *calculator, std::vector<std::string> *parametersWithValues)
+  {
+    calculator->params.setDefaultValues();
+    calculator->params.update(*parametersWithValues, calculator->scenarioIndexesByUuid, calculator->scenarios, calculator->nodeIndexesByUuid, calculator->agencyIndexesByUuid, calculator->lineIndexesByUuid, calculator->serviceIndexesByUuid, calculator->modeIndexesByShortname, calculator->dataSourceIndexesByUuid);
+    calculator->params.birdDistanceAccessibilityEnabled = true;
+
+    if (calculator->params.isCompleteForCalculation())
+    {
+      // find OdTrip if provided:
+      bool foundOdTrip{false};
+
+      calculator->origin = &calculator->params.origin;
+      calculator->destination = &calculator->params.destination;
+      calculator->odTrip = nullptr;
+
+      if (calculator->params.odTripUuid.is_initialized() && calculator->odTripIndexesByUuid.count(calculator->params.odTripUuid.get()))
+      {
+        calculator->odTrip = calculator->odTrips[calculator->odTripIndexesByUuid[calculator->params.odTripUuid.get()]].get();
+        foundOdTrip = true;
+        std::cout << "od trip uuid " << calculator->odTrip->uuid << std::endl;
+        std::cout << "dts " << calculator->odTrip->departureTimeSeconds << std::endl;
+        calculator->origin = calculator->odTrip->origin.get();
+        calculator->destination = calculator->odTrip->destination.get();
+      }
+
+      if (calculator->params.alternatives)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void benchmarkCurrentParams(TrRouting::Calculator *calculator)
+  {
+    std::ofstream benchmarkResultsFile("benchmarkResults.txt");
+
+    int nbIter = 10;
+    double results[nbIter];
+    for (int i = 0; i < nbIter; i++)
+    {
+      auto start = std::chrono::high_resolution_clock::now();
+      calculator->alternativesRouting();
+      auto end = std::chrono::high_resolution_clock::now();
+      results[i] = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() * 1e-9;
+    }
+    benchmarkResultsFile << "Execution time results: " << std::endl;
+    double resultSum = 0;
+    for (int i = 0; i < nbIter; i++)
+    {
+      benchmarkResultsFile << "Iteration " << i << ": " << std::fixed << results[i] << std::setprecision(9) << " seconds" << std::endl;
+      resultSum += results[i];
+    }
+    benchmarkResultsFile << "Average execution time: "<< std::fixed << resultSum / (double)nbIter << std::setprecision(9) << " seconds per routing calculation" << std::endl;
+    benchmarkResultsFile.close();
+  }
+};
+
+TEST_F(BenchmarkCSATests, TestGetFilePath)
+{
+
+  Parameters algorithmParams = setupAlgorithmParams();
+
+  Calculator calculator(algorithmParams);
+
+  if (!updateCalculatorFromCache(&calculator))
+    ASSERT_EQ(true, false);
+
+  calculator.params.setDefaultValues();
+
+  // TODO Shouldn't need to do this, but we do for now, those benchmarks are not the same as those in this program though. Here we loop and have microseconds precision.
+  calculator.algorithmCalculationTime.start();
+  calculator.benchmarking.clear();
+
+  std::vector<std::string> parametersWithValues = createCalculationQuery();
+
+  if (updateCalculatorParams(&calculator, &parametersWithValues))
+  {
+    benchmarkCurrentParams(&calculator);
+    ASSERT_EQ(true, true);
+  }
+  else
+  {
+    ASSERT_EQ(true, false);
+  }
+}
