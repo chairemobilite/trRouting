8c9a25e70480b4b716ddd4ccf2f0b8e9aa3bd380
Geneviève Bastien <gbastien@versatic.net>
2021-12-09 13:07:44 -0500

    csa: Move node and schedules internal data initialization code
    Internal data for nodes and schedules were initialized in the methods
    that fetched the nodes and schedules respectively, but this code should
    be executed whenever nodes and schedules data are modified, not just
    when fetched from cache.
    
    This code is moved to a single initializeCalculationData method in
    initializations.cpp file and make public for unit tests to call.

--- connection_scan_algorithm/src/preparations.cpp
+++ connection_scan_algorithm/src/preparations.cpp
@@ -13,47 +13,9 @@ namespace TrRouting
     params.cacheFetcher->getStops(stops, stopIndexesByUuid, nodeIndexesByUuid, params, customPath);
   }*/
 
-
   int Calculator::updateNodesFromCache(Parameters& params, std::string customPath)
   {
-    int ret = params.cacheFetcher->getNodes(nodes, nodeIndexesByUuid, stationIndexesByUuid, params, customPath);
-    if (ret < 0)
-    {
-      return ret;
-    }
-
-    nodesTentativeTime.clear();
-    nodesTentativeTime.shrink_to_fit();
-    nodesTentativeTime.resize(nodes.size());
-    nodesReverseTentativeTime.clear();
-    nodesReverseTentativeTime.shrink_to_fit();
-    nodesReverseTentativeTime.resize(nodes.size());
-    nodesAccessTravelTime.clear();
-    nodesAccessTravelTime.shrink_to_fit();
-    nodesAccessTravelTime.resize(nodes.size());
-    nodesAccessDistance.clear();
-    nodesAccessDistance.shrink_to_fit();
-    nodesAccessDistance.resize(nodes.size());
-    nodesEgressTravelTime.clear();
-    nodesEgressTravelTime.shrink_to_fit();
-    nodesEgressTravelTime.resize(nodes.size());
-    nodesEgressDistance.clear();
-    nodesEgressDistance.shrink_to_fit();
-    nodesEgressDistance.resize(nodes.size());
-    forwardJourneysSteps.clear();
-    forwardJourneysSteps.shrink_to_fit();
-    forwardJourneysSteps.resize(nodes.size());
-    forwardEgressJourneysSteps.clear();
-    forwardEgressJourneysSteps.shrink_to_fit();
-    forwardEgressJourneysSteps.resize(nodes.size());
-    reverseJourneysSteps.clear();
-    reverseJourneysSteps.shrink_to_fit();
-    reverseJourneysSteps.resize(nodes.size());
-    reverseAccessJourneysSteps.clear();
-    reverseAccessJourneysSteps.shrink_to_fit();
-    reverseAccessJourneysSteps.resize(nodes.size());
-
-    return ret;
+    return params.cacheFetcher->getNodes(nodes, nodeIndexesByUuid, stationIndexesByUuid, params, customPath);
   }
 
   int Calculator::updateDataSourcesFromCache(Parameters& params, std::string customPath)
@@ -113,7 +75,7 @@ namespace TrRouting
 
   int Calculator::updateSchedulesFromCache(Parameters& params, std::string customPath)
   {
-    int ret = params.cacheFetcher->getSchedules(
+    return params.cacheFetcher->getSchedules(
       trips,
       lines,
       paths,
@@ -131,88 +93,7 @@ namespace TrRouting
       params,
       customPath
     );
-    if (ret < 0)
-    {
-      return ret;
-    }
 
-    tripsEnabled.clear();
-    tripsEnabled.shrink_to_fit();
-    tripsEnabled.resize(trips.size());
-    tripsUsable.clear();
-    tripsUsable.shrink_to_fit();
-    tripsUsable.resize(trips.size());
-    tripsEnterConnection.clear();
-    tripsEnterConnection.shrink_to_fit();
-    tripsEnterConnection.resize(trips.size());
-    tripsExitConnection.clear();
-    tripsExitConnection.shrink_to_fit();
-    tripsExitConnection.resize(trips.size());
-    tripsEnterConnectionTransferTravelTime.clear();
-    tripsEnterConnectionTransferTravelTime.shrink_to_fit();
-    tripsEnterConnectionTransferTravelTime.resize(trips.size());
-    tripsExitConnectionTransferTravelTime.clear();
-    tripsExitConnectionTransferTravelTime.shrink_to_fit();
-    tripsExitConnectionTransferTravelTime.resize(trips.size());
-
-    std::cout << forwardConnections.size() << " connections" << std::endl; 
-
-    //int benchmarkingStart = algorithmCalculationTime.getEpoch();
-
-    int lastConnectionIndex = forwardConnections.size() - 1;
-
-    forwardConnectionsIndexPerDepartureTimeHour = std::vector<int>(32, -1);
-    reverseConnectionsIndexPerArrivalTimeHour   = std::vector<int>(32, lastConnectionIndex);
-    
-    int hour {0};
-    int i = 0;
-    for (auto & connection : forwardConnections)
-    {
-      while (std::get<connectionIndexes::TIME_DEP>(*connection) >= hour * 3600 && forwardConnectionsIndexPerDepartureTimeHour[hour] == -1 && hour < 32)
-      {
-        forwardConnectionsIndexPerDepartureTimeHour[hour] = i;
-        //std::cout << hour << ":" << i << ":" << std::get<connectionIndexes::TIME_DEP>(connection) << std::endl;
-        hour++;
-      }
-      i++;
-    }
-
-    hour = 31;
-    i = 0;
-    for (auto & connection : reverseConnections)
-    {
-      while (std::get<connectionIndexes::TIME_ARR>(*connection) <= hour * 3600 && reverseConnectionsIndexPerArrivalTimeHour[hour] == lastConnectionIndex && hour >= 0)
-      {
-        reverseConnectionsIndexPerArrivalTimeHour[hour] = i;
-        //std::cout << hour << ":" << i << ":" << std::get<connectionIndexes::TIME_ARR>(connection) << std::endl;
-        hour--;
-      }
-      i++;
-    }
-
-    for (int h = 0; h < 32; h++)
-    {
-      if (forwardConnectionsIndexPerDepartureTimeHour[h] == -1)
-      {
-        forwardConnectionsIndexPerDepartureTimeHour[h] = lastConnectionIndex;
-      }
-      //std::cout << h << ": " << forwardConnectionsIndexPerDepartureTimeHour[h] << std::endl;
-    }
-    /*for (int h = 0; h < 32; h++)
-    {
-      std::cout << h << ": " << reverseConnectionsIndexPerArrivalTimeHour[h] << std::endl;
-    }*/
-
-    /*for (auto & node : nodes)
-    {
-      std::cout << node.toString() << std::endl;
-      for (int transferableNodeIdx : node.transferableNodesIdx)
-      {
-        std::cout << "    " << transferableNodeIdx << " (" << nodes[transferableNodeIdx].get()->uuid << ")" << std::endl;
-      }
-      std::cout << std::endl;
-    }*/
-    return ret;
   }
 
   int Calculator::prepare()
@@ -314,6 +195,7 @@ namespace TrRouting
       // not yet implemented
     }
     
+    initializeCalculationData();
     std::cout << "preparing nodes tentative times, trips enter connections and journeys..." << std::endl;
     return ret;
   }
