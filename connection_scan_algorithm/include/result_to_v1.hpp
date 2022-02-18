2de3a5f4f71542672b19e2bf53ae349387f36457
Geneviève Bastien <gbastien@versatic.net>
2022-02-18 11:56:03 -0500

    Add a converter from result to v1 API
    Add a base class to convert result to json response. A first
    implementation is added to convert to the V1 result.
    
    Add unit tests to test the conversion with simple result classes.

---
+++ connection_scan_algorithm/include/result_to_v1.hpp
@@ -0,0 +1,23 @@
+#ifndef TR_RESULT_TO_V1_RESPONSE
+#define TR_RESULT_TO_V1_RESPONSE
+
+#include "json.hpp"
+
+namespace TrRouting
+{
+
+  class RouteParameters;
+  class RoutingResultNew;
+
+  /**
+   * @brief Convert a result object to a json object for the version 1 trRouting API, as described in docs/API.yml
+   */
+  class ResultToV1Response {
+  public:
+    static nlohmann::json resultToJsonString(RoutingResultNew& result, RouteParameters& params);
+    static nlohmann::json noRoutingFoundResponse(RouteParameters& params);
+  };
+
+}
+
+#endif // TR_RESULT_TO_V1_RESPONSE
