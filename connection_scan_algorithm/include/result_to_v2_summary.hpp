588dfd1520e4aa27cc97d8d881d4ac4d352cff1a
Geneviève Bastien <gbastien@versatic.net>
2022-09-20 16:13:14 -0400

    Implement the `v2/summary` endpoint
    Fixes #177
    
    This endpoint does exactly the same calculation as the `v2/route`
    endpoint. Only the result converter is changed.
    
    The ResultToV2SummaryResponse converter takes the routing result and
    creates the json response with the details for each line used. It also
    calculates the number of times a line is encountered in an alternative.

---
+++ connection_scan_algorithm/include/result_to_v2_summary.hpp
@@ -0,0 +1,24 @@
+#ifndef TR_RESULT_TO_V2_SUMMARY_RESPONSE
+#define TR_RESULT_TO_V2_SUMMARY_RESPONSE
+
+#include "json.hpp"
+#include "routing_result.hpp"
+
+namespace TrRouting
+{
+
+  class RouteParameters;
+
+  /**
+   * @brief Convert a result object to a json object for the version 2 trRouting summary API
+   */
+  class ResultToV2SummaryResponse {
+  public:
+    static nlohmann::json resultToJsonString(RoutingResult& result, RouteParameters& params);
+    static nlohmann::json noRoutingFoundResponse(RouteParameters& params, NoRoutingReason noRoutingReason);
+  };
+
+}
+
+
+#endif // TR_RESULT_TO_V2_SUMMARY_RESPONSE
