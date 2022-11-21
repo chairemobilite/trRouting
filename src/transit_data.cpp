7366494b4b803a409fb2f57278320626548d9817
Yannick Brosseau <yannick.brosseau@gmail.com>
2022-11-21 17:22:30 -0500

    Add comments and constants to generateConnectionsIteratorCache function

--- src/transit_data.cpp
+++ src/transit_data.cpp
@@ -135,9 +135,12 @@ namespace TrRouting {
     return generateForwardAndReverseConnections(connections);
   }
 
+  const int CONNECTION_ITERATOR_CACHE_BEGIN_HOUR = 0;
+  const int CONNECTION_ITERATOR_CACHE_END_HOUR = 32;
+
   std::vector<std::shared_ptr<Connection>>::const_iterator TransitData::getForwardConnectionsBeginAtDepartureHour(int hour) const
   {
-    if (hour > 32 || hour  < 0) {
+    if (hour > CONNECTION_ITERATOR_CACHE_END_HOUR || hour  < CONNECTION_ITERATOR_CACHE_BEGIN_HOUR) {
       return forwardConnections.cend();
     }
 
@@ -146,7 +149,7 @@ namespace TrRouting {
 
   std::vector<std::shared_ptr<Connection>>::const_iterator TransitData::getReverseConnectionsBeginAtArrivalHour(int hour) const
   {
-    if (hour > 32 || hour  < 0) {
+    if (hour > CONNECTION_ITERATOR_CACHE_END_HOUR || hour  < CONNECTION_ITERATOR_CACHE_BEGIN_HOUR) {
       return reverseConnections.cend();
     }
 
@@ -155,31 +158,40 @@ namespace TrRouting {
 
   // Create a cache with a begin iterator which match the connection closest to the specified hour
   void TransitData::generateConnectionsIteratorCache() {
-    int currentHour = 0;
+    int currentHour = CONNECTION_ITERATOR_CACHE_BEGIN_HOUR;
+
+    // Create first part of the forward cache
+    // For each hour, we save the iterator matching the connection which as a departure time bigger
+    // than this hour
     for (std::vector<std::shared_ptr<Connection>>::const_iterator ite = forwardConnections.cbegin();
          ite != forwardConnections.cend();
          ite++) {
+      // We can have a gap of multiple hours between 2 connections, so the current iterator
+      // can be valid for multiple hour slots
       while ((*ite)->getDepartureTime() >= currentHour * 3600) {
-        //forwardConnectionsBeginIteratorCache[currentHour] = ite;
         forwardConnectionsBeginIteratorCache.push_back(ite);
         currentHour++;
       }
     }
+
     //Finish filling forward cache
-    for (;currentHour < 32; currentHour++) {
+    //We reached the end of the forwardConnections in the previously, so here we
+    //fill the rest of the forward cache with end iterator
+    for (;currentHour < CONNECTION_ITERATOR_CACHE_END_HOUR; currentHour++) {
       forwardConnectionsBeginIteratorCache.push_back(forwardConnections.cend());
     }
 
-    currentHour = 31;
+    // Create the reverse cache
+    currentHour = CONNECTION_ITERATOR_CACHE_END_HOUR - 1;
     for (std::vector<std::shared_ptr<Connection>>::const_iterator ite = reverseConnections.cbegin();
          ite != reverseConnections.cend();
          ite++) {
-      while ((*ite)->getArrivalTime() <= currentHour * 3600 && currentHour > 0) {
-        //forwardConnectionsBeginIteratorCache[currentHour] = ite;
+      // Fill cache with same iterator if we have multi hour gap between connection
+      while ((*ite)->getArrivalTime() <= currentHour * 3600 && currentHour > CONNECTION_ITERATOR_CACHE_BEGIN_HOUR) {
         reverseConnectionsBeginIteratorCache.insert(reverseConnectionsBeginIteratorCache.begin(),ite);
         currentHour--;
       }
-    }    
+    }
   }
 
   int TransitData::generateForwardAndReverseConnections(const std::vector<std::shared_ptr<Connection>> &connections)
