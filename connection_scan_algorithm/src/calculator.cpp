dc9795a21025057b887e34544ec61918a3e4ffe8
Yannick Brosseau <yannick.brosseau@gmail.com>
2022-12-02 12:47:29 -0500

    Split AllNodes codepath in calculate function
    Create both calculateSingle and calculateAllNodes. In later diff we will be able
    to call the right one directly, for example in the alternativeRouting case.
    
    Future work: handle initialDepartureTimeSeconds not globally, simplify reset() based
    on each code path. Probably other optimization

--- connection_scan_algorithm/src/calculator.cpp
+++ connection_scan_algorithm/src/calculator.cpp
@@ -13,85 +13,60 @@ namespace TrRouting
   
   std::unique_ptr<RoutingResult> Calculator::calculate(RouteParameters &parameters, bool resetAccessPaths, bool resetFilters) {
 
+    if (params.returnAllNodesResult) {
+      return calculateAllNodes(parameters, resetAccessPaths, resetFilters);
+    } else {
+      return calculateSingle(parameters, resetAccessPaths, resetFilters);
+    }
+  }
+
+  std::unique_ptr<SingleCalculationResult> Calculator::calculateSingle(RouteParameters &parameters, bool resetAccessPaths, bool resetFilters) {
     reset(parameters, resetAccessPaths, resetFilters);
 
-    std::unique_ptr<RoutingResult> result;
-    
-    int bestArrivalTime {MAX_INT};
-    int bestDepartureTime {-1};
-    std::optional<std::reference_wrapper<const Node>> bestEgressNode;
-    std::optional<std::reference_wrapper<const Node>> bestAccessNode;
-
-    // TODO Could they be local to each section?
-    std::unordered_map<Node::uid_t, JourneyStep> forwardEgressJourneysSteps;
-    std::unordered_map<Node::uid_t, JourneyStep> reverseAccessJourneysSteps;
-
+    std::unique_ptr<SingleCalculationResult> result;
 
     if (departureTimeSeconds > -1 && parameters.isForwardCalculation())
     {
       
       initialDepartureTimeSeconds = departureTimeSeconds; // set initial departure time so we can find the latest possible departure time with reverse calculation later and still know the initial waiting time
+      int bestArrivalTime {MAX_INT};
+      std::optional<std::reference_wrapper<const Node>> bestEgressNode;
+      //TODO With the TODO later that forwardJourneyStep is not necessary, we can also drop this variable
+      std::unordered_map<Node::uid_t, JourneyStep> forwardEgressJourneysSteps;
 
-      if (params.returnAllNodesResult) {
-        forwardCalculationAllNodes(parameters, forwardEgressJourneysSteps);
+      auto resultCalculation = forwardCalculation(parameters, forwardEgressJourneysSteps);
+      if (resultCalculation.has_value()) {
+        bestArrivalTime = std::get<0>(*resultCalculation);
+        bestEgressNode = std::get<1>(*resultCalculation);
+      }
 
-        spdlog::debug("-- forward calculation all nodes -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
-        calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+      spdlog::debug("-- forward calculation -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
+      calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+        
+      if (bestArrivalTime < MAX_INT)
+      {
+        spdlog::debug("bestArrivalTime after forward journey: {}", bestArrivalTime);
+          
+        arrivalTimeSeconds = bestArrivalTime;
+          
+        for (auto & egressFootpath : egressFootpaths) // reset nodes reverse tentative times with new arrival time:
+        {
+          nodesReverseTentativeTime[egressFootpath.node.uid] = arrivalTimeSeconds - egressFootpath.time;
+        }
 
-        result = forwardJourneyStepAllNodes(parameters, forwardEgressJourneysSteps);
-
-        spdlog::debug("-- forward journey all nodes -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
-        calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+        result = calculateSingleReverse(parameters);
+          
       }
       else
       {
-        auto resultCalculation = forwardCalculation(parameters, forwardEgressJourneysSteps);
-        if (resultCalculation.has_value()) {
-          bestArrivalTime = std::get<0>(*resultCalculation);
-          bestEgressNode = std::get<1>(*resultCalculation);
-        }
+        //TODO This will always throw an exception since to get here bestEgressNode must be invalid
+        //TODO We can probably just remove the function forwardJourneyStep completely
+        result = forwardJourneyStep(parameters, bestEgressNode, forwardEgressJourneysSteps);
 
-        spdlog::debug("-- forward calculation -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
+        assert(false); // See TODO
+        spdlog::debug("-- forward journey -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
         calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
-        
-        if (bestArrivalTime < MAX_INT)
-        {
-          spdlog::debug("bestArrivalTime after forward journey: {}", bestArrivalTime);
           
-          arrivalTimeSeconds = bestArrivalTime;
-          
-          for (auto & egressFootpath : egressFootpaths) // reset nodes reverse tentative times with new arrival time:
-          {
-            nodesReverseTentativeTime[egressFootpath.node.uid] = arrivalTimeSeconds - egressFootpath.time;
-          }
-
-          auto resultCalculationRev = reverseCalculation(parameters, reverseAccessJourneysSteps);
-          if (resultCalculationRev) {
-            bestDepartureTime = std::get<0>(*resultCalculationRev);
-            bestAccessNode = std::get<1>(*resultCalculationRev);
-          }
-
-          spdlog::debug("-- reverse calculation -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
-          calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
-
-          result = reverseJourneyStep(parameters, bestDepartureTime, bestAccessNode, reverseAccessJourneysSteps);
-
-          spdlog::debug("-- reverse journey -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
-          calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
-          
-        }
-        else
-        {
-          //TODO This will always throw an exception since to get here bestEgressNode must be invalid
-          //TODO We can probably just remove the function forwardJourneyStep completely
-          result = forwardJourneyStep(parameters, bestEgressNode, forwardEgressJourneysSteps);
-
-          assert(false); // See TODO
-          spdlog::debug("-- forward journey -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
-          calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
-          
-        }
-        
       }
     }
     else if (arrivalTimeSeconds > -1)
@@ -104,34 +79,85 @@ namespace TrRouting
         const Trip & trip = tripIte.second;
         tripsQueryOverlay[trip.uid].usable = true;
       }
+      result = calculateSingleReverse(parameters);
 
-      if (params.returnAllNodesResult) { //TODO Temporary until we split calculate()
+    }
 
-        reverseCalculationAllNodes(parameters, reverseAccessJourneysSteps);
+    return result;
+  }
 
-        spdlog::debug("-- reverse calculation --  {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
-        calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+  // To be called only by calculateSingle, depends on preparations steps done there
+  std::unique_ptr<SingleCalculationResult> Calculator::calculateSingleReverse(RouteParameters &parameters) {
 
-        result = reverseJourneyStepAllNodes(parameters, reverseAccessJourneysSteps);
-      } else {
+    std::unique_ptr<SingleCalculationResult> result;
 
-        auto resultCalculation = reverseCalculation(parameters, reverseAccessJourneysSteps);
-        if (resultCalculation) {
-          bestDepartureTime = std::get<0>(*resultCalculation);
-          bestAccessNode = std::get<1>(*resultCalculation);
-        }
+    int bestDepartureTime {-1};
+    std::optional<std::reference_wrapper<const Node>> bestAccessNode;
+    std::unordered_map<Node::uid_t, JourneyStep> reverseAccessJourneysSteps;
 
-        spdlog::debug("-- reverse calculation --  {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
-        calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
-        result = reverseJourneyStep(parameters, bestDepartureTime, bestAccessNode, reverseAccessJourneysSteps);
+    auto resultCalculation = reverseCalculation(parameters, reverseAccessJourneysSteps);
+    if (resultCalculation) {
+      bestDepartureTime = std::get<0>(*resultCalculation);
+      bestAccessNode = std::get<1>(*resultCalculation);
+    }
+
+    spdlog::debug("-- reverse calculation --  {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
+    calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+    result = reverseJourneyStep(parameters, bestDepartureTime, bestAccessNode, reverseAccessJourneysSteps);
+
+    spdlog::debug("-- reverse journey -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
+    calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+
+    return result;
+  }
+
+  std::unique_ptr<AllNodesResult> Calculator::calculateAllNodes(RouteParameters &parameters, bool resetAccessPaths, bool resetFilters) {
+    reset(parameters, resetAccessPaths, resetFilters);
+
+    std::unique_ptr<AllNodesResult> result;
+
+    if (departureTimeSeconds > -1 && parameters.isForwardCalculation())
+    {
+      std::unordered_map<Node::uid_t, JourneyStep> forwardEgressJourneysSteps;
+
+      initialDepartureTimeSeconds = departureTimeSeconds; // set initial departure time so we can find the latest possible departure time with reverse calculation later and still know the initial waiting time
+
+      forwardCalculationAllNodes(parameters, forwardEgressJourneysSteps);
+
+      spdlog::debug("-- forward calculation all nodes -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
+      calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+
+      result = forwardJourneyStepAllNodes(parameters, forwardEgressJourneysSteps);
+
+      spdlog::debug("-- forward journey all nodes -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
+      calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+
+    }
+    else if (arrivalTimeSeconds > -1)
+    {
+      std::unordered_map<Node::uid_t, JourneyStep> reverseAccessJourneysSteps;
+
+      departureTimeSeconds = -1;
+      initialDepartureTimeSeconds = -1;
+      //TODO maybe we can do something different in that case, like a query flag
+      // we need to make all trips usable when not coming from forward result because reverse calculation, by default, checks for usableTrips
+      for (auto && tripIte : transitData.getTrips()) {
+        const Trip & trip = tripIte.second;
+        tripsQueryOverlay[trip.uid].usable = true;
       }
+
+      reverseCalculationAllNodes(parameters, reverseAccessJourneysSteps);
+
+      spdlog::debug("-- reverse calculation --  {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
+      calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
+
+      result = reverseJourneyStepAllNodes(parameters, reverseAccessJourneysSteps);
 
       spdlog::debug("-- reverse journey -- {} microseconds", algorithmCalculationTime.getDurationMicrosecondsNoStop() - calculationTime);
       calculationTime = algorithmCalculationTime.getDurationMicrosecondsNoStop();
     }
 
     return result;
-    
   }
   
 }
